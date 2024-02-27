a9798a0f5392925a66d5bb2f7458634809bb5fce
joseph6655 <josephsassine66@gmail.com>
2024-02-27 11:25:34 +0200

    new changes

--- src/comps/Navbar.js
+++ src/comps/Navbar.js
@@ -4,18 +4,19 @@ import Link from 'next/link'
 const Navbar = () => {
   return (
     <nav className="navbar">
-      
+      <div className='nav-content'>
         <div className ="logo">
           <img src="/creoshiftlogo.jpg"/>
           </div>
-      <ul className="navList">
-        <li className="navItem"><Link href="/" className="navLink">Home</Link>
-        </li>
-        <li className="navItem"><Link href="/about"className="navLink">About</Link>
-        </li>
-        <li className="navItem"><Link href="/contact" className="navLink">Contact</Link>
-        </li>
-      </ul>
+         <ul className="navList">
+           <li className="navItem"><Link href="/" className="navLink">Home</Link>
+           </li>
+           <li className="navItem"><Link href="/about"className="navLink">About</Link>
+           </li>
+           <li className="navItem"><Link href="/contact" className="navLink">Contact</Link>
+           </li>
+         </ul>
+      </div>
       
     </nav>
   );
--- src/comps/SectionImage.js
+++ src/comps/SectionImage.js
@@ -1,17 +1,15 @@
-import React from 'react'
+import React from 'react';
 
 const SectionImage = () => {
   return (
     <section className="section-image">
       <div className="image-container">
-        <img src="desk.png"  />
+        <img src="desk.png" alt="Desk" />
+        <div className="card-content">Great web design without functionality is like a sports car with no engine.</div>
       </div>
-      <div className="section-image">
       <div className="image-container">
-        <img src="comp.png"  />
-        </div>
-        
-        
+        <img src="comp.png" alt="Computer" />
+        <div className="card-content">Computers are like Old Testament gods; lots of rules and no mercy.</div>
       </div>
     </section>
   );
