cf8521054df2bb882f8983aba0a7ff589358b96d
joseph6655 <josephsassine66@gmail.com>
2024-02-28 14:15:07 +0200

    new changes

--- src/comps/Navbar.js
+++ src/comps/Navbar.js
@@ -2,32 +2,38 @@ import React, { useState } from 'react';
 import Link from 'next/link';
 
 const Navbar = () => {
-  const [menuOpen, setMenuOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-  const toggleMenu = () => {
-    setMenuOpen(!menuOpen);
+  const toggleNav = () => {
+    setIsOpen(!isOpen);
   };
 
   return (
     <nav className="navbar">
-      <div className='nav-content'>
+      <div className="nav-content">
         <div className="logo">
-          <img src="/creoshiftlogo.jpg" alt="Creoshift Logo" />
+          <img src="/creoshiftlogo.jpg" alt="Logo" />
         </div>
-        <div className={`menu-toggle ${menuOpen ? 'open' : ''}`} onClick={toggleMenu}>
-          <div className="bar"></div>
-          <div className="bar"></div>
-          <div className="bar"></div>
-        </div>
-        <ul className={`navList ${menuOpen ? 'open' : ''}`}>
+        <button className="burger-button" onClick={toggleNav}>
+          <div className="burger-line"></div>
+          <div className="burger-line"></div>
+          <div className="burger-line"></div>
+        </button>
+        <ul className={`navList ${isOpen ? 'showNav' : ''}`}>
           <li className="navItem">
-            <Link href="/" className="navLink">Home</Link>
+            <Link href="/" className="navLink">
+              Home
+            </Link>
           </li>
           <li className="navItem">
-            <Link href="/about" className="navLink">About</Link>
+            <Link href="/about" className="navLink">
+              About
+            </Link>
           </li>
           <li className="navItem">
-            <Link href="/contact" className="navLink">Contact</Link>
+            <Link href="/contact" className="navLink">
+              Contact
+            </Link>
           </li>
         </ul>
       </div>
