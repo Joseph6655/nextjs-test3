12ed89117226373d04a92d1f8b1ff510695b0fb7
joseph6655 <josephsassine66@gmail.com>
2024-02-28 12:43:42 +0200

    new changes

--- src/comps/Form.js
+++ src/comps/Form.js
@@ -3,6 +3,7 @@ import React from 'react'
 const Form = () => {
   return (
     <form className="form">
+     <div className="title"><h2>Enter your information here </h2></div>
       <div className="form-group">
         <label htmlFor="name">Name:</label>
         <input type="text" id="name" name="name" placeholder="  Enter your name" />
@@ -13,7 +14,7 @@ const Form = () => {
       </div>
       <div className="form-group">
         <label htmlFor="message">Message:</label>
-        <textarea id="message" name="message" rows="4" placeholder="  Enter your message"></textarea>
+        <input type="text" id="name" name="name" placeholder="  Enter your message" />
       </div>
       <div className="button-container">
       <button type="submit">Submit</button></div>
