4f49ea61cd44aa25cee27ba29097d65a5dcce2ca
joseph6655 <josephsassine66@gmail.com>
2024-03-06 16:20:08 +0200

    new changes

--- src/comps/Form.js
+++ src/comps/Form.js
@@ -3,10 +3,10 @@ import React from 'react';
 const Form = () => {
   return (
     <form className="form max-w-md mx-auto ">
-      <div className="title text-center mb-2 text-3xl">
+      <div className="title text-center mb-2 text-3xl ">
         <h2 className="font-bold mt-11 md:ml-4 lg:ml-0">Enter your information here</h2>
       </div>
-      <div className="form-group mb-4">
+      <div className="form-group mb-4 ">
         <label htmlFor="name" className="block mt-8">Name:</label>
         <input type="text" id="name" name="name" placeholder="Enter your name" className="w-full border rounded-md py-2 px-3" />
       </div>
--- src/comps/SectionImage.js
+++ src/comps/SectionImage.js
@@ -4,15 +4,15 @@ const SectionImage = () => {
   return (
     <section className="section-image mt-10">
       <div className="container mx-auto flex flex-col md:flex-row items-center justify-center">
-        <div className="image-container mx-6 mb-6 md:mb-4 md:w-80">
+        <div className="image-container mx-6 mb-6 md:mb-4 md:w-80 relative">
           <img src="desk.png" alt="Desk" />
-          <div className="card-content relative text-center mt-12 bg-black bg-opacity-50 p-5 left-1/2 md:mt-4">
+          <div className="card-content absolute bottom-0 left-0 w-full bg-black bg-opacity-50 p-5 text-white text-center">
             Great web design without functionality like a sports car with no engine.
           </div>
         </div>
-        <div className="image-container mx-6 md:w-80">
+        <div className="image-container mx-6 md:w-80 mb-5 relative">
           <img src="comp.png" alt="Computer" />
-          <div className="card-content relative text-center mt-12 bg-black bg-opacity-50 p-5 left-1/2 ">
+          <div className="card-content absolute bottom-0 left-0 w-full bg-black bg-opacity-50 p-5 text-white text-center">
             Computers are like Old Testament gods; lots of rules and no mercy.
           </div>
         </div>
