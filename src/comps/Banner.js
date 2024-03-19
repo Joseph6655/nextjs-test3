def36f1693535e879839a27b3b7d68cd4bc1fa5f
joseph6655 <josephsassine66@gmail.com>
2024-03-19 11:55:35 +0200

    new changes

--- src/comps/Banner.js
+++ src/comps/Banner.js
@@ -3,7 +3,7 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/bundle';
 
-const Banner = ({ image, cardContent, textColor }) => {
+const Banner = ({ images, cardContent, textColor }) => {
   const swiperParams = {
     loop: true,
     autoplay: {
@@ -19,18 +19,20 @@ const Banner = ({ image, cardContent, textColor }) => {
     <section id="banner" className="py-16 bg-gray-100">
       <div className="content">
         <Swiper {...swiperParams}>
-          <SwiperSlide>
-            <div className="banner-item relative">
-              <div className="image-container relative w-full">
-                <img src={image} alt="Background" className="w-full h-screen" />
-                <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 p-8 rounded-lg">
-                  <div className={`card-content text-white text-center mt-12 bg-black bg-opacity-50 smaller-text-xl md:text-5xl px-4 py-2 rounded-lg mb-10 ${textColor}`}>
-                    <h2>{cardContent}</h2>
+          {Array.isArray(images) && images.map((image, index) => (
+            <SwiperSlide key={index}>
+              <div className="banner-item relative">
+                <div className="image-container relative w-full">
+                  <img src={image} alt={`Background ${index}`} className="w-full h-screen" />
+                  <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 p-8 rounded-lg">
+                    <div className={`card-content text-white text-center mt-12 bg-black bg-opacity-50 smaller-text-xl md:text-5xl px-4 py-2 rounded-lg mb-10 ${textColor}`}>
+                      <h2>{cardContent}</h2>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          </SwiperSlide>
+            </SwiperSlide>
+          ))}
         </Swiper>
       </div>
     </section>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Head from 'next/head';
-import Banner from '@/comps/Banner';
+import Banner from '@/comps/Banner'; // Import the Banner component
 import Navbar from '@/comps/Navbar';
 import Footer from '@/comps/Footer';
 import Form from '@/comps/Form';
@@ -11,8 +11,6 @@ import 'swiper/css';
 import 'swiper/css/bundle';
 
 const HomePage = () => {
-  const bannerImageSource = '/banner.png';
-
   const imagesData = [
     { imageSource: '/desk.png', cardContent: 'Great web design without functionality like a sports car with no engine.' },
     { imageSource: '/comp.png', cardContent: 'Computers are like Old Testament gods; lots of rules and no mercy.' }
@@ -24,8 +22,11 @@ const HomePage = () => {
       <Head>
         <title>Home page</title>
       </Head>
-     
-      <Banner image={bannerImageSource} cardContent="Welcome to Home page" />
+      {/* Add the Banner component with two additional images */}
+      <Banner 
+        images={['/banner.png', '/banner.png', '/about.png']}
+        cardContent="Welcome to Home page" 
+      />
       <Form/> 
      
       <div className="grid grid-cols-1 ml-20 mr-20 md:grid-cols-2 gap-0 md:w-1/2 justify-center" style={{ margin: '0 auto' }}>
